26676956eab404c3fa484a0d97d60db0c250a59f
rdalmau23 <mr.yertz01@gmail.com>
2025-06-23 09:43:45 +0200

    feat: search bar created

--- frontend/src/app/pages/user-list/user-list.ts
+++ frontend/src/app/pages/user-list/user-list.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
@@ -11,23 +12,40 @@ import autoTable from 'jspdf-autotable';
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
 export class UserList implements OnInit {
   users: User[] = [];
+  filteredUsers: User[] = [];
+  searchTerm: string = '';
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
+      this.filteredUsers = users;
     });
   }
 
+  filterUsers(): void {
+    const term = this.searchTerm.trim().toLowerCase();
+    if (!term) {
+      this.filteredUsers = this.users;
+      return;
+    }
+    this.filteredUsers = this.users.filter(user =>
+      user.name.toLowerCase().includes(term) ||
+      user.surname.toLowerCase().includes(term) ||
+      user.id.toLowerCase().includes(term) ||
+      user.email.toLowerCase().includes(term)
+    );
+  }
+
   downloadExcel(): void {
-    const worksheet = XLSX.utils.json_to_sheet(this.users);
+    const worksheet = XLSX.utils.json_to_sheet(this.filteredUsers);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Usuaris');
     const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
@@ -37,7 +55,7 @@ export class UserList implements OnInit {
 
   downloadPDF(): void {
     const doc = new jsPDF();
-    const tableData = this.users.map(user => [user.name, user.surname, user.email, user.id]);
+    const tableData = this.filteredUsers.map(user => [user.name, user.surname, user.email, user.id]);
 
     autoTable(doc, {
       head: [['Nom', 'Cognoms', 'Email', 'DNI']],
@@ -45,9 +63,5 @@ export class UserList implements OnInit {
     });
 
     doc.save('usuaris.pdf');
-
-    
   }
 }
-
-
